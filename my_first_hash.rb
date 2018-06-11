11b3516460088692a232b006a91b887a8cb93498
Vishal Sharma <vsh1819@gmail.com>
2018-06-11 11:48:37 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,8 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
+  
 
 end
 
