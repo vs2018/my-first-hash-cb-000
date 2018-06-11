12d3503099868e58b7f0c1ee1d47a76ac0a59c85
Vishal Sharma <vsh1819@gmail.com>
2018-06-11 11:49:13 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,6 +28,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4
 
 end
